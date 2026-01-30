a5f5635ca9bb47372ec89ac8418d7a81aea2a31e
Jason Kim <jasonjiseop.kim@gmail.com>
2026-01-30 19:00:10 +0900

    feat: spatial partitioning for colliders

---
+++ Physics/PhysicsWorld.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "Physics.h"
+#include "SpatialGrid.h"
+
+namespace Physics
+{
+    class PhysicsWorld
+    {
+    public:
+		PhysicsWorld() : spatialGrid(5.0f) {} // 5.0 unit cell size by default
+
+        void AddBody(std::shared_ptr<RigidBody> rb, std::shared_ptr<Collider> col)
+        {
+            bodies.push_back(rb);
+            colliders.push_back(col);
+        }
+
+		// Ran in fixed timesteps, so needs interpolation during rendering with rb->GetInterpolatedTransform(alpha)
+        void Update(float dt)
+        {
+            // 1. Integrate RigidBodies
+            for (auto& rb : bodies)
+            {
+                // Apply Gravity
+                //if (rb->mass > 0) rb->AddForce(Vector3(0, -9.8f * rb->mass, 0));
+                rb->Integrate(dt);
+            }
+
+            // 2. Update Spatial Partition
+            spatialGrid.Clear();
+            for (auto& col : colliders)
+            {
+                spatialGrid.Insert(col.get());
+                col->isColliding = false;
+            }
+
+            // 3. Collision Detection (Using Spatial Partition)
+            for (auto& col : colliders)
+            {
+                auto candidates = spatialGrid.GetCandidates(col.get());
+                AABB myBounds = col->GetWorldAABB();
+
+                for (auto* other : candidates)
+                {
+                    if (myBounds.Intersects(other->GetWorldAABB()))
+                    {
+                        ResolveCollision(col.get(), other);
+                    }
+                }
+            }
+        }
+
+    private:
+        void ResolveCollision(Collider* a, Collider* b)
+        {
+            // Basic impulse resolution or logic
+            a->isColliding = true;
+            b->isColliding = true;
+
+            // Simple bounce (velocity reflection)
+            Vector3 relVel = b->attachedBody->velocity - a->attachedBody->velocity;
+            // TODO: Full impulse resolution code would go here
+        }
+
+        std::vector<std::shared_ptr<RigidBody>> bodies;
+        std::vector<std::shared_ptr<Collider>> colliders;
+        SpatialGrid spatialGrid;
+    };
+}
