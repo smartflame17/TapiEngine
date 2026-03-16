3ec6f70135ca5fbf10c7e5e735e976da4d642cd0
Jason Kim <103112980+smartflame17@users.noreply.github.com>
2026-03-16 18:16:29 +0900

    fix: align matrix-to-transform decomposition with ImGuizmo rotation handling

--- Graphics/Drawable/Mesh.h
+++ Graphics/Drawable/Mesh.h
@@ -13,7 +13,14 @@ public:
 	Mesh(Graphics& gfx, std::vector<std::unique_ptr<IBindable>> bindPtrs);
 	void Draw(Graphics& gfx, DirectX::FXMMATRIX accumulatedTransform) const noexcept(!IS_DEBUG);
 	DirectX::XMMATRIX GetTransformXM() const noexcept override;
+	void SetTransform(const Transform& transform) noexcept = delete;
+	void SetExternalTransformMatrix(DirectX::FXMMATRIX matrix) noexcept = delete;
 
 private:
-	mutable DirectX::XMFLOAT4X4 transform;
+	mutable DirectX::XMFLOAT4X4 transform = {
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	};
 };
