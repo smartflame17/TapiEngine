5e2b116dda38717fbbda810e4c7fe00e195c704b
Jason Kim <jasonjiseop.kim@gmail.com>
2026-03-18 18:46:31 +0900

    fix: grayscale channel forced to RGBA32

--- Graphics/IBindable/Texture.cpp
+++ Graphics/IBindable/Texture.cpp
@@ -2,14 +2,22 @@
 #include "../../ErrorHandling/GraphicsExceptionMacros.h"
 #include <WICTextureLoader.h> // Provided by DirectXTK
 
-Texture::Texture(Graphics& gfx, const std::wstring& path)
+Texture::Texture(Graphics& gfx, const std::wstring& path, UINT slot)
+	:
+	slot(slot)
 {
 	HRESULT hr;
-	// CreateWICTextureFromFile handles creating the Texture2D and the ResourceView
-	GFX_THROW_FAILED(DirectX::CreateWICTextureFromFile(
+	// Force WIC textures to RGBA so grayscale images don't sample as red-only in the shader.
+	GFX_THROW_FAILED(DirectX::CreateWICTextureFromFileEx(
 		GetDevice(gfx),
 		GetContext(gfx),
 		path.c_str(),
+		0u,
+		D3D11_USAGE_DEFAULT,
+		D3D11_BIND_SHADER_RESOURCE,
+		0u,
+		0u,
+		DirectX::WIC_LOADER_FORCE_RGBA32,
 		nullptr,
 		&pTextureView
 	));
@@ -17,6 +25,5 @@ Texture::Texture(Graphics& gfx, const std::wstring& path)
 
 void Texture::Bind(Graphics& gfx) noexcept
 {
-	// Bind to slot 0 of the Pixel Shader
-	GetContext(gfx)->PSSetShaderResources(0u, 1u, pTextureView.GetAddressOf());
+	GetContext(gfx)->PSSetShaderResources(slot, 1u, pTextureView.GetAddressOf());
 }
--- Graphics/IBindable/Texture.h
+++ Graphics/IBindable/Texture.h
@@ -5,8 +5,9 @@ class Texture : public IBindable
 {
 public:
 	// Loads image from file path using DirectXTK
-	Texture(Graphics& gfx, const std::wstring& path);
+	Texture(Graphics& gfx, const std::wstring& path, UINT slot = 0u);
 	void Bind(Graphics& gfx) noexcept override;
 protected:
 	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> pTextureView;
+	UINT slot = 0u;
 };
