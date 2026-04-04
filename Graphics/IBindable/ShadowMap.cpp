1e23bb597d35c285b49daa7f474594bccda4243d
Jason Kim <jasonjiseop.kim@gmail.com>
2026-04-04 17:16:04 +0900

    feat: shadow map supports cubemap (6 faces) for point

--- Graphics/IBindable/ShadowMap.cpp
+++ Graphics/IBindable/ShadowMap.cpp
@@ -1,10 +1,11 @@
 #include "ShadowMap.h"
 #include "../../ErrorHandling/GraphicsExceptionMacros.h"
 
-ShadowMap::ShadowMap(Graphics& gfx, UINT size, UINT slot)
+ShadowMap::ShadowMap(Graphics& gfx, UINT size, UINT slot, Type type)
 	:
 	size(size),
-	slot(slot)
+	slot(slot),
+	type(type)
 {
 	HRESULT hr;
 
@@ -12,24 +13,57 @@ ShadowMap::ShadowMap(Graphics& gfx, UINT size, UINT slot)
 	textureDesc.Width = size;
 	textureDesc.Height = size;
 	textureDesc.MipLevels = 1u;
-	textureDesc.ArraySize = 1u;
+	textureDesc.ArraySize = type == Type::TextureCube ? 6u : 1u;
 	textureDesc.Format = DXGI_FORMAT_R24G8_TYPELESS;
 	textureDesc.SampleDesc.Count = 1u;
 	textureDesc.Usage = D3D11_USAGE_DEFAULT;
 	textureDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL | D3D11_BIND_SHADER_RESOURCE;
+	textureDesc.MiscFlags = type == Type::TextureCube ? D3D11_RESOURCE_MISC_TEXTURECUBE : 0u;
 	GFX_THROW_FAILED(GetDevice(gfx)->CreateTexture2D(&textureDesc, nullptr, &pTexture));
 
-	D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
-	dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-	dsvDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
-	dsvDesc.Texture2D.MipSlice = 0u;
-	GFX_THROW_FAILED(GetDevice(gfx)->CreateDepthStencilView(pTexture.Get(), &dsvDesc, &pDepthStencilView));
+	if (type == Type::TextureCube)
+	{
+		pDepthStencilViews.reserve(6u);
+		for (UINT face = 0u; face < 6u; face++)
+		{
+			D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
+			dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+			dsvDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DARRAY;
+			dsvDesc.Texture2DArray.MipSlice = 0u;
+			dsvDesc.Texture2DArray.FirstArraySlice = face;
+			dsvDesc.Texture2DArray.ArraySize = 1u;
+
+			Microsoft::WRL::ComPtr<ID3D11DepthStencilView> pDepthStencilView;
+			GFX_THROW_FAILED(GetDevice(gfx)->CreateDepthStencilView(pTexture.Get(), &dsvDesc, &pDepthStencilView));
+			pDepthStencilViews.push_back(std::move(pDepthStencilView));
+		}
+	}
+	else
+	{
+		D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
+		dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		dsvDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+		dsvDesc.Texture2D.MipSlice = 0u;
+
+		Microsoft::WRL::ComPtr<ID3D11DepthStencilView> pDepthStencilView;
+		GFX_THROW_FAILED(GetDevice(gfx)->CreateDepthStencilView(pTexture.Get(), &dsvDesc, &pDepthStencilView));
+		pDepthStencilViews.push_back(std::move(pDepthStencilView));
+	}
 
 	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
 	srvDesc.Format = DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
-	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	srvDesc.Texture2D.MostDetailedMip = 0u;
-	srvDesc.Texture2D.MipLevels = 1u;
+	if (type == Type::TextureCube)
+	{
+		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURECUBE;
+		srvDesc.TextureCube.MostDetailedMip = 0u;
+		srvDesc.TextureCube.MipLevels = 1u;
+	}
+	else
+	{
+		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+		srvDesc.Texture2D.MostDetailedMip = 0u;
+		srvDesc.Texture2D.MipLevels = 1u;
+	}
 	GFX_THROW_FAILED(GetDevice(gfx)->CreateShaderResourceView(pTexture.Get(), &srvDesc, &pShaderResourceView));
 }
 
@@ -40,15 +74,31 @@ void ShadowMap::Bind(Graphics& gfx) noexcept
 
 void ShadowMap::BeginWrite(Graphics& gfx) noexcept
 {
-	ID3D11ShaderResourceView* const nullSrv = nullptr;
-	GetContext(gfx)->PSSetShaderResources(slot, 1u, &nullSrv);
-	GetContext(gfx)->OMSetRenderTargets(0u, nullptr, pDepthStencilView.Get());
-	GetContext(gfx)->ClearDepthStencilView(pDepthStencilView.Get(), D3D11_CLEAR_DEPTH, 1.0f, 0u);
-	gfx.SetViewport(static_cast<float>(size), static_cast<float>(size));
+	if (!pDepthStencilViews.empty())
+	{
+		BeginWrite(gfx, pDepthStencilViews.front().Get());
+	}
+}
+
+void ShadowMap::BeginWriteFace(Graphics& gfx, UINT face) noexcept
+{
+	if (face < pDepthStencilViews.size())
+	{
+		BeginWrite(gfx, pDepthStencilViews[face].Get());
+	}
 }
 
 void ShadowMap::Unbind(Graphics& gfx) noexcept
 {
 	ID3D11ShaderResourceView* const nullSrv = nullptr;
 	GetContext(gfx)->PSSetShaderResources(slot, 1u, &nullSrv);
+}
+
+void ShadowMap::BeginWrite(Graphics& gfx, ID3D11DepthStencilView* pTargetView) noexcept
+{
+	ID3D11ShaderResourceView* const nullSrv = nullptr;
+	GetContext(gfx)->PSSetShaderResources(slot, 1u, &nullSrv);
+	GetContext(gfx)->OMSetRenderTargets(0u, nullptr, pTargetView);
+	GetContext(gfx)->ClearDepthStencilView(pTargetView, D3D11_CLEAR_DEPTH, 1.0f, 0u);
+	gfx.SetViewport(static_cast<float>(size), static_cast<float>(size));
 }
--- Graphics/IBindable/ShadowMap.h
+++ Graphics/IBindable/ShadowMap.h
@@ -4,15 +4,25 @@
 class ShadowMap : public IBindable
 {
 public:
-	ShadowMap(Graphics& gfx, UINT size, UINT slot = 2u);
+	enum class Type
+	{
+		Texture2D,
+		TextureCube
+	};
+
+	ShadowMap(Graphics& gfx, UINT size, UINT slot = 2u, Type type = Type::Texture2D);
 	void Bind(Graphics& gfx) noexcept override;
 	void BeginWrite(Graphics& gfx) noexcept;
+	void BeginWriteFace(Graphics& gfx, UINT face) noexcept;
 	void Unbind(Graphics& gfx) noexcept;
 
 private:
+	void BeginWrite(Graphics& gfx, ID3D11DepthStencilView* pTargetView) noexcept;
+
 	UINT size = 0u;
 	UINT slot = 0u;
+	Type type = Type::Texture2D;
 	Microsoft::WRL::ComPtr<ID3D11Texture2D> pTexture;
-	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> pDepthStencilView;
+	std::vector<Microsoft::WRL::ComPtr<ID3D11DepthStencilView>> pDepthStencilViews;
 	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> pShaderResourceView;
 };
